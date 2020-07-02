52c1b4f219f0ebb2831aca11590dd341307234d7
MohsenKazemizade <e.m.kazemizade@gmail.com>
2020-07-02 16:57:47 +0430

    simple Request & Body Validation + missing lines(users-server)

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-// @rout     Get api/users
-// @desc     Test rout
+// @rout     post api/users
+// @desc     Register user
 // @access   Public
-router.get('/', (req, res) => res.send('Users rout'));
+router.post(
+  '/',
+  [
+    body('name', 'لطفا نام خود را وارد کنید!').not().isEmpty(),
+    body('email', 'لطفا آدرس ایمیل خود را به طور صحیح وارد کنید!').isEmail(),
+    body(
+      'password',
+      'لطفا رمز عبور خود را بیش تر از ۶ کارکتر وارد کنید!'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User rout');
+  }
+);
 
 module.exports = router;
