a56ee6f04fa4b1c8e5bee9b1f2ce97767e5d29dd
MohsenKazemizade <e.m.kazemizade@gmail.com>
2020-07-02 19:32:57 +0430

    User Registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 // @rout     post api/users
 // @desc     Register user
@@ -15,12 +18,49 @@ router.post(
       'لطفا رمز عبور خود را بیش تر از ۶ کارکتر وارد کنید!'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User rout');
+
+    const { name, email, password } = req.body;
+
+    try {
+      //see if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'آدرس ایمیل قبل ثبت شده است.' }] });
+      }
+
+      //Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      //Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      //Return jsonwebtoken
+
+      res.send('User Register');
+    } catch (err) {}
   }
 );
 
