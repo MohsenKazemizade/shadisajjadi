bc2a5a33c66df34291378f01b5bd5d3cee5ba20b
MohsenKazemizade <e.m.kazemizade@gmail.com>
2020-07-02 20:31:37 +0430

    custom auth middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 // @rout     Get api/auth
 // @desc     Test rout
 // @access   Public
-router.get('/', (req, res) => res.send('Auth rout'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('server errror');
+  }
+});
 
 module.exports = router;
