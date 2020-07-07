5bbc8e416d275352017b0182a5fd871240e93426
MohsenKazemizade <e.m.kazemizade@gmail.com>
2020-07-07 16:21:49 +0430

    Get profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -113,4 +113,27 @@ router.get('/', async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+// @rout     Get api/profile/user/:user_id
+// @desc     Get profile by user id
+// @access   Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+    if (!profile)
+      return res
+        .status(400)
+        .json({ msg: 'پروفایلی برای این کاربر وجود ندارد' });
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res
+        .status(400)
+        .json({ msg: 'پروفایلی برای این کاربر وجود ندارد' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
